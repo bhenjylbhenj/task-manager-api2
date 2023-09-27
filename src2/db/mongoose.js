07bb15e1a2cb5e358a01d0e1d98cdee93647ca4b
bhenjylbhenj <38584109+bhenjylbhenj@users.noreply.github.com>
2023-09-27 11:14:45 +0800

    deleting src2 to create a new one

--- src2/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-try {
-    mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api2', {
-        useNewUrlParser: true
-    })
-    // console.log('connected to db');    
-} catch (error) {
-    console.log(error);
-}
